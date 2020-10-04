71db96d7bb6b5a9a7c21e747ef38d54a9b6128d7
Dany Galan <danygalan1334@gmail.com>
2020-10-04 14:23:13 -0600

    Proyecto Terminado I Unidad

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -6,10 +6,46 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int opcion = 0;
+    
+    while(true) {
     system("cls");
 
-    // starShip();
-    // snake();
+        cout << "*********************";
+        cout << endl;
+        cout << "    MENU DE JUEGO"; 
+        cout << endl;
+        cout << "*********************";
+        cout << endl;
+        cout << endl;
+        cout << "Seleccione un juego"; 
+        cout << endl << endl;
+        cout << "1 - Starship" << endl << endl;
+        cout << "2 - Snake" << endl << endl;
+        cout << "0 - Salir" << endl << endl ;
+        cout << "Ingrese un numero del menu para seleccionar un juego:";
+        cin >> opcion;
 
+        switch (opcion)
+        {
+        case 1: 
+            starShip();            
+            break;
+        case 2:
+            snake();
+            break;
+                
+        default:
+            break;
+        }
+
+        system("pause");
+        cout << endl;
+
+        if (opcion == 0) {
+            break;
+        }
+    
     return 0;
+    }
 }
